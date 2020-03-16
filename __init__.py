d83a1ac9c2025480f47ebcf99ec7dec3b8725084
aiventures <49552433+aiventures@users.noreply.github.com>
2020-03-16 21:25:30 +0100

    Initialize Repo

---
+++ __init__.py
@@ -0,0 +1,20 @@
+"""module that allows for calculation useful for photography
+   commonly used abbreviations (also check out constants definitions):
+   k: Aperture Number 
+   sensor_type: sensor dimensions
+   megapixels: sensor resolution
+   f:focal length in mm
+   dof: Depth of field
+   fov: Field of view
+   iso: Sensor Sensitivity
+   crop: Crop Factor sensor
+   lambda: Wavelength in nm
+   m: (image) magnification
+   t: Exposure time (in seconds)
+   EV: Exposure Value
+   D: Diopters of closeup lens in 1/map
+   extension: extension of bellows/macro rings in mm
+   alpha: Ray incident angle (relative to normal of lens surface)
+   parameter with_keys determines whether result will be given in dictionary alongside with 
+   input values (=True) or not (=False)
+"""
